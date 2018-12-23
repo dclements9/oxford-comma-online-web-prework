9732f56a16a101d5d7b17b2a398c49fae536b7b7
dclements <dclements@protonmail.com>
2018-12-23 18:51:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    ##### THis one
-    array[-1].insert(0, "and ")
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  when 3
+
+  else
+
+    
   end
-  array.join(", ")
 end
