bb2508459f7116e9c16ce3d7e738150494c8d13d
dclements <dclements@protonmail.com>
2018-12-23 19:00:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,8 @@ def oxford_comma(array)
   when 2
     array.join(" and ")
   else
-    array[-1].insert(0, "and ")
+    #array[-1].insert(0, "and ")
+    array.insert(-1, "and ")
     array.join(", ")
   end
 end
